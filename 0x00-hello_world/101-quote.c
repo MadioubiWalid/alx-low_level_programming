9e8fdf8212809a90f425d1961afc2df39c5ef865
MadioubiWalid <madioubi.walid@gmail.com>
2023-03-16 11:22:17 -0700

    hi

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
-#include <unistd.h> 
-#include <sys/syscall.h> 
+#include <stdio.h> 
+#include <unistd.h>
+#include <sys/syscall.h>
 /**
  * main - programs prints a line without puts and printf
- * Return: Always 1 (Success)
+ * Return:  1 (Success)
  */
 int main(void) 
-{ 
-    const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
-    syscall(SYS_write, 1, msg, sizeof(msg)); 
-    return (1); 
+{
+	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
+	syscall(SYS_write, 1, msg, sizeof(msg)); 
+	return (1); 
 }
