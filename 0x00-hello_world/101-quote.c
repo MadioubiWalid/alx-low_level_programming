76babd71dcd87824b248317f8be115fa0222dc96
MadioubiWalid <madioubi.walid@gmail.com>
2023-03-16 11:26:18 -0700

    enfin

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h> 
 #include <unistd.h>
-#include <sys/syscall.h>
 /**
  * main - programs prints a line without puts and printf
  * Return:  1 (Success)
  */
 int main(void) 
 {
-	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
-	syscall(SYS_write, 1, msg, sizeof(msg)); 
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout); 
 	return (1); 
 }
