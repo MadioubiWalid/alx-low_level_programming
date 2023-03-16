9ba773c8ca3af96d8339d894c3785f330f9f564e
MadioubiWalid <madioubi.walid@gmail.com>
2023-03-16 11:50:01 -0700

    pleaaase

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <unistd.h>
 /**
  * main - programs prints a line without puts and printf
  * Return:  1 (Success)
  */
-int main(void) 
+int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); 
 	return (1);
