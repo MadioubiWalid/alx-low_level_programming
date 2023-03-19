b70e4f57b4c743387ace0b3234a56952f63e169c
MadioubiWalid <madioubi.walid@gmail.com>
2023-03-19 11:25:22 -0700

    first

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,7 +14,7 @@ int main(void)
 	if ( n > 0 ) {                 
         printf("%d is negative\n" ); 
     }
-    else if ( age == 0 ) {           
+    else if ( n == 0 ) {           
         printf("%d is zero\n" );       
     }
     else {
