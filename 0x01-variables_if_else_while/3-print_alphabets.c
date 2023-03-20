84a38c715a96d93d9813e1378e26e98c027c803a
MadioubiWalid <madioubi.walid@gmail.com>
2023-03-20 11:11:40 -0700

    alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - print alphabets
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 97;
+	int j = 65;
+
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+	while (j <= 90)
+	{
+		putchar(j);
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
